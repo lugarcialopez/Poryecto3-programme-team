f52e45039f1cc2c1d843ebbbfc9b76adbebfb828
lugarcialopez <120428904+lugarcialopez@users.noreply.github.com>
2023-02-06 18:12:12 -0500

    js de carreras
    ejemplo de js de carreras

---
+++ assets/data/carreras.js
@@ -0,0 +1,15 @@
+export var x= "x";
+
+export var carrerasData = {
+    "carreras": [
+        {
+            "id": 1,
+            "nombre": "Ingenieria Biomedica",
+            "años": "5",
+            "universidades": "PUCP, UPCH, UNMSM, UPC" ,
+            "descripcion": "Combinación de los campos de ingeniería y medicina para fabricar productos o rediseñar aparatos que puedan ayudar en la salud de las personas.",
+            "campo laboral": ""
+        }
+
+]
+}
