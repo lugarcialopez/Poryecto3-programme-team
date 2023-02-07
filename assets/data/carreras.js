ae70623e23abd74bd1007265358344e85dcdb32c
Abi0523 <120526978+Abi0523@users.noreply.github.com>
2023-02-06 19:25:07 -0500

    commit
    borrador de diana

--- assets/data/carreras.js
+++ assets/data/carreras.js
@@ -9,6 +9,20 @@ export var carrerasData = {
             "universidades": "PUCP, UPCH, UNMSM, UPC" ,
             "descripcion": "Combinación de los campos de ingeniería y medicina para fabricar productos o rediseñar aparatos que puedan ayudar en la salud de las personas.",
             "campo laboral": ""
+        },
+        {
+            "id": 2,
+            "nombre": "Ingenieria Ambiental",
+            "años": "5",
+            "universidades": "MSM, PUCP, UNMSM, UPCA, UNI" ,
+            "descripcion": "su enfoque es para mejorar los procesos o diseñar cosas que sean mas eficientes y desperdicien menos dinero , tiempo , recursos en bruto, mano de obra y energía, siguiendo los estándares y regulaciones de seguridad.",
+            "campo laboral":"Los ingenieros industriales trabajan en una variedad de entornos diferentes. Esto podría incluir observar cómo funciona un proceso en una fábrica o examinar los flujos de trabajo en un hospital. Cada dato se puede tomar y examinar usando una computadora para resolver problemas.",
+            "A que se dedica": " Ellos trabajan en todas las etapas de producción y procesamiento. Esto puede implicar el diseño de un producto o proceso desde el principio o la adaptación y actualización, expansión o reconfiguración de uno ya existente.",
+            "Cursos" : "Evaluación de impacto ambiental. Restauración y remediación ambiental.Energías renovables.Resolución de conflictos socio ambientales.",
+
+           
+
+
         }
 
 ]
