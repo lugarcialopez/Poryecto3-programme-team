943b5cbff123ccb97431336fca4b386673262148
lugarcialopez <120428904+lugarcialopez@users.noreply.github.com>
2023-02-06 19:23:37 -0500

    borrador js carreras
    Borrador js carreras

--- assets/data/carreras.js
+++ assets/data/carreras.js
@@ -6,9 +6,15 @@ export var carrerasData = {
             "id": 1,
             "nombre": "Ingenieria Biomedica",
             "años": "5",
-            "universidades": "PUCP, UPCH, UNMSM, UPC" ,
+            "universidades/institutos": "PUCP, UPCH, UNMSM, UPC" ,
             "descripcion": "Combinación de los campos de ingeniería y medicina para fabricar productos o rediseñar aparatos que puedan ayudar en la salud de las personas.",
-            "campo laboral": ""
+            "campo laboral": "Hospitales, clínicas de rehabilitación, gestión en la compra de equipamiento e infraestructura de centros de salud, ingeniero diseñador y/o evaluador de tecnología médica, administrador y supervisor de servicios de telesalud.",
+            "especialidades": "Señales e imágenes biomédicas, ingeniería de Tejidos y Biomateriales, biomecánica y rehabilitación, ingeniería clínica (crear servicios clínicos)",
+            "cursos": "Matemáticas, Física, Biología, Quimica",
+            "sueldo": "Recien egresados ganan S/1,891 a S/5,398 mensuales. El sueldo maximo es de S/11,622 mensuales",
+            "estadisticas": "Se encuentra en el puesto 7 de la lista de los 10 mejores trabajos, que elabora el New YorkTimes junto a la CNN, y se proyecta con un crecimiento mayor a 61% en los próximos 10 años. En el caso de Perú, se espera que un egresado con más de 4 años de experiencia pueda ganar más de cinco mil soles, es decir, más de 70 mil soles anuales.",
+            "profesionales": "Rossana Rivas",
+
         }
 
 ]
