9b2a05c64f8d2314e63d0eb3b3d6f949e3ac31b5
lugarcialopez <120428904+lugarcialopez@users.noreply.github.com>
2023-02-08 19:28:24 -0500

    avance 8/2
    agregando más datos

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,57 @@
 import {carrerasData} from "./data/carreras.js"
 
 var data= carrerasData;
-$(document).ready{
-    function () {
-        for (let index=0; index<20 ; index ++) {
-            mostrarCarreras(index);
+$(document).ready(
+    function() {
+        for ( let index=0; index<7 ; index ++) {
+
+            if(($("#myselect").val()=="general")||($("#myselect").val()==data.carreras[index].ubicacion))
+                {
+                    mostrarCarreras(index);
+                }
+
+            
         }
-    }
-};
+            
+    },
+    $("#myselect").change( function() {
+
+    }),
+    $("#myselect").change( function () {
+        $("#contenedorGeneral").children().not("#contenedorGeneral").remove();
+        for (let index = 0; index < 7; index++) {
+    
+        if(($("#myselect").val()=="general")||($( "#myselect" ).val()==data.carreras[index].ubicacion))
+           {
+            mostrarCarreras(index);
+           }
+               
+                    
+         }
+    })
+        
+);
+    
+
 
 function mostrarCarreras(index) {
     let div=$("<div></div>");
+    div.addClass("ejemplos-carreras")
     let imagen = $("<img></img>");
-    imagen.sttr("src", data.pokemon[index].img);
+    imagen.sttr("src", data.carreras[index].img);
+    imagen.addClass("img-carreras");
     div.append(imagen);
-    
+
+    let nombreCarreras=$("<p></p>");
+    nombreCarreras.append(data.carreras[index].nombre);
+    nombreCarreras.addClass("nombre");
+    div.append(nombreCarreras);
+    let ubicacionCarreras=$("<p></p>");
+    ubicacionCarreras.append(data.carreras[index].tipo);
+    ubicacionCarreras.addClass("tipo");
+    div.append(ubicacionCarreras);
+
+    $('#contenedorGeneral').append(div);  
 }
+    
+
